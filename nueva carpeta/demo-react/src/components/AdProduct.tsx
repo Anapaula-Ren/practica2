216efcc7d91591b24ed494f430b167a879733a95
Anapaula-Ren <L23100201@nlaredo.tecnm.mx>
2026-09-17 19:03:17 -0500

    feat: product form

--- nueva carpeta/demo-react/src/components/AdProduct.tsx
+++ nueva carpeta/demo-react/src/components/AdProduct.tsx
@@ -1,31 +1,26 @@
-import { useState } from "react"
-import {type Product} from '../components/ProductCards.tsx'
+import {useState} from 'react'
+import { type Product } from "../components/ProductCards"
+import { ProductCard } from '../components/ProductCards'
  
-export const AddProduct = () => {
-    const [products, setProducts] = useState<Product[]>([])
-   
-    const handleAddProduct = () =>
-    {
-        const newProduct: Product =
-        {
-            id: Date.now().toString(),
-            titulo: "Laptop HP 15-dw3003la",
-            categoria: "Computadoras",
-            descripcion: "Laptop HP 15-dw3003la, Intel Core i5-1135G7, 8GB RAM, 256GB SSD, Windows 11 Home",
+export const AddProducts = () => {
+    const [products,setProducts] = useState<Product[]>([])
+    const handleAddProducts = () => {
+        const newProduct: Product = {
+            id:Date.now().toString(),
+            titulo: 'Producto 1',
+            categoria: 'Categoria 1',
+            descripcion: 'Descripcion del producto 1',
             precio: 24999
         }
         setProducts([...products,newProduct])
     }
- 
     return (
         <div>
-            <button onClick={handleAddProduct}>Add Product</button>
+            <button onClick={handleAddProducts}>Add Product</button>
             {
                 products.map(product=>
-                <ProductCards key={product.id} product = {product}/>)
+                <ProductCard key={product.id} product = {product}/>)
             }
         </div>
     )
 }
- 
- 
